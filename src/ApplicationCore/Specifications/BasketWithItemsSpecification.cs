134d9aa959dfad23421a0fb10ca5ae5765e33683
Luka Sostar <l.sostar@emakina.hr>
2022-09-07 12:09:10 +0200

    initial commit

---
+++ src/ApplicationCore/Specifications/BasketWithItemsSpecification.cs
@@ -0,0 +1,22 @@
+﻿using Ardalis.Specification;
+using Microsoft.eShopWeb.ApplicationCore.Entities.BasketAggregate;
+
+namespace Microsoft.eShopWeb.ApplicationCore.Specifications;
+
+public sealed class BasketWithItemsSpecification : Specification<Basket>, ISingleResultSpecification
+{
+    //wtf? is this a method? what's it returning? what's 
+    public BasketWithItemsSpecification(int basketId)
+    {
+        Query
+            .Where(b => b.Id == basketId)
+            .Include(b => b.Items);
+    }
+
+    public BasketWithItemsSpecification(string buyerId)
+    {
+        Query
+            .Where(b => b.BuyerId == buyerId)
+            .Include(b => b.Items);
+    }
+}
