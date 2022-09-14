0edbb5bd4857f7b53ac1fffcabb6cff1c4b4c092
Luka Sostar <l.sostar@emakina.hr>
2022-09-14 13:31:06 +0200

    added writing to blob to AzureFunction

--- src/AzureFunctions/OrderItemsReserver.cs
+++ src/AzureFunctions/OrderItemsReserver.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System.Text;
 using System.Threading.Tasks;
+using Azure.Storage.Blobs;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
@@ -17,19 +18,15 @@ namespace AzureFunctions.Properties;
 
 public static class OrderItemsReserver
 {
+    private static string _CONNECTION_STRING = "DefaultEndpointsProtocol=https;AccountName=shokistorage1;AccountKey=Q9cGwxBDSZ6T8H8EmJEPQIO9SFkejrdqxejfeWtJv/oZdG2OUrPaZIabbdSfRjgbFrx9BE+zfG/6+AStYMCflQ==;EndpointSuffix=core.windows.net";
+    private static string _CONTAINER_NAME = "cloudxcontainer";
+    
     [FunctionName("OrderItemsReserver")]
-    public static async Task<IActionResult> RunAsync([HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req, 
-                                                     ILogger log)
+    public static async Task<IActionResult> RunAsync([HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req, ILogger log)
     {
         try
         {
-            //test if im sending the correct json
-            // req.Body.Position = 0;
-            // var reader = new StreamReader(req.Body, Encoding.UTF8);
-            // var body = await reader.ReadToEndAsync().ConfigureAwait(false);
-            // req.Body.Position = 0;
-            // log.LogInformation($"*** {body}");
-            
+           
             string bodyString = await new StreamReader(req.Body).ReadToEndAsync();
             
             //this won't work because i failed to deserialize nested objects. need to annotate model, which i'll skip for now.
@@ -42,6 +39,9 @@ public static class OrderItemsReserver
             
             
             var message = $"OrderItemsReserver: order received: {id} by user {buyerId}";
+
+            var blobName = $"{buyerId}.{id}.{DateTime.Now.ToString("yyyy'-'MM'-'dd'T'HH':'mm':'ss")}.json";
+            await WriteDataToBlob(blobName, bodyString, log);
             
             
             log.LogInformation(message);
@@ -49,9 +49,36 @@ public static class OrderItemsReserver
         }
         catch (Exception e)
         {
-            log.LogError("OrderItemsReserver: order not sent");
-            return new BadRequestObjectResult($"OrderItemsReserver: bad order: {e}");
+            log.LogError($"OrderItemsReserver: order not sent: {e}");
+            return new BadRequestObjectResult("OrderItemsReserver: error");
         }
 
+    }
+
+    private static async Task WriteDataToBlob(string blobName, string data, ILogger logger)
+    {
+        try
+        {
+            //init client
+            BlobServiceClient blobServiceClient = new BlobServiceClient(_CONNECTION_STRING);
+            BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient(_CONTAINER_NAME);
+            await containerClient.CreateIfNotExistsAsync();
+        
+        
+            // get blob client and write
+            BlobClient blobClient = containerClient.GetBlobClient(blobName);
+            using (var stream = new MemoryStream(System.Text.Encoding.UTF8.GetBytes(data)))
+            {
+                await blobClient.UploadAsync(stream);    
+            }
+            logger.LogInformation($"WriteDataToBlob: success");
+        }
+        catch (Exception e)
+        {
+            logger.LogError($"WriteDataToBlob: whoopsie: {e}");
+            throw;
+        }
+        
+        
     }
 }
